a4003dab6e575a2b42d988e4470a4ea71ab3004d
BulatIdrisov <156697834+BulatIdrisov@users.noreply.github.com>
2024-11-18 23:11:23 +0300

    Update main.py
    added dotenv

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import os
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
 
 import db as db
 import keyboards as kb
 import casino
 
-bot = Bot("TOKEN")
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
 storage = MemoryStorage()
 dp = Dispatcher(bot,storage=storage)
-ADMIN_ID = 'ADMIN_ID'
+ADMIN_ID = os.getenv('ADMIN_ID')
 admins = [ADMIN_ID]
 async def on_startup(_):
     await db.db_start()
