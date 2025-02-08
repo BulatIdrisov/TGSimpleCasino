d06faa51b658d217f6db9aa3f1c0f5e12733fea9
BulatIdrisov <156697834+BulatIdrisov@users.noreply.github.com>
2025-02-08 23:49:41 +0300

    updating the logic
    Optimized the code. Now all bets are placed in a single block of code (this allowed to get rid of 55 extra lines of code). The status category "Game" has been introduced, which starts when the "Play" button is pressed. I also made minor edits. That's all!

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class NewOrder(StatesGroup):
     message = State()
     replenish_ID = State()
     replenish_balance = State()
+    game = State()
 
 @dp.message_handler(text='Сообщение админу')
 async def message(message: types.Message):
@@ -38,16 +39,16 @@ async def message_op(message: types.Message, state: FSMContext):
     else:
         await state.finish()
         await message.answer("Сообщение успешно отправлено!", reply_markup=kb.main)
-        await bot.send_message(ADMIN_ID, 'Новое сообщение!: '+message.text)
+        await bot.send_message(ADMIN_ID, f'Новое сообщение! {message.text}')
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     if message.from_user.id == int(ADMIN_ID):
         await db.cmd_start_db(message.from_user.id, message.from_user.first_name)
-        await message.answer(f'{message.from_user.first_name}, Добро пожаловать в казинак!', reply_markup=kb.main_admin)
+        await message.answer(f'{message.from_user.first_name}, Добро пожаловать в казинак!\n(Если при нажатии на кнопку ставки ничего не происходит, то введите команду /start)', reply_markup=kb.main_admin)
     else:
         await db.cmd_start_db(message.from_user.id, message.from_user.first_name)
-        await message.answer(f'{message.from_user.first_name}, Добро пожаловать в казинак!',reply_markup=kb.main)
+        await message.answer(f'{message.from_user.first_name}, Добро пожаловать в казинак!\n(Если при нажатии на кнопку ставки ничего не происходит, то введите команду /start)',reply_markup=kb.main)
 
 @dp.message_handler(text='Пополнение')
 async def replenish2(message: types.Message):
@@ -78,86 +79,46 @@ async def replenish_balance(message: types.Message, state: FSMContext):
         user_id = user_data['id']
         balance_sum = message.text
         db.replenish(user_id, balance_sum)
-        await message.answer(f"Успешное пополнение баланса пользователя: {user_id} на: {balance_sum}", reply_markup=kb.main_admin)
-        await bot.send_message(user_id, f"Ваш баланс успешно пополнен на: {balance_sum}")
+        await message.answer(f"Успешное пополнение баланса пользователя: {user_id} на {balance_sum}", reply_markup=kb.main_admin)
+        await bot.send_message(user_id, f"Ваш баланс успешно пополнен на {balance_sum}")
         await state.finish()
-
-
-
-
-
 
 
 @dp.message_handler(text='Играть')
 async def game(message: types.Message):
-    await message.answer(f'Выберите ставку\nВаш баланс: {db.balance(message.from_user.id)}', reply_markup=kb.game)
+    await NewOrder.game.set()
+    await message.answer(f'Выберите ставку или введите свою! \nВаш баланс: {db.balance(message.from_user.id)}', reply_markup=kb.game)
 
-@dp.message_handler(text='10')
-async def bet10(message: types.Message):
-
-    a = casino.bet(10)
-    sum = a[0]
-    slots = a[1]
-    if db.balance(message.from_user.id)-10>=0:
-        if sum>0:
-            await db.update_balance(message.from_user.id, sum)
-            await message.answer("Аппарат показал: " + slots + "\nВы выиграли: " + str(sum), reply_markup=kb.game)
-            await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
-
-
-        else:
-            await db.update_balance(message.from_user.id, sum)
-            await message.answer("Аппарат показал: " + slots + "\nВы проиграли: "+str(sum*(-1)),reply_markup=kb.game)
-            await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
+@dp.message_handler(state=NewOrder.game)
+async def bet(message: types.Message, state: FSMContext):
+    if message.text == 'Выйти':
+        await message.answer("Выход в главное меню!", reply_markup=kb.main)
+        await state.finish()
     else:
-        await message.answer("Недостаточно денег на балансе: "+str(db.balance(message.from_user.id)))
-
-@dp.message_handler(text='100')
-async def bet10(message: types.Message):
-
-    a = casino.bet(100)
-    sum = a[0]
-    slots = a[1]
-    if db.balance(message.from_user.id)-100>=0:
-        if sum>0:
-            await db.update_balance(message.from_user.id, sum)
-            await message.answer("Аппарат показал: " + slots + "\nВы выиграли: " + str(sum), reply_markup=kb.game)
-            await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
+        try:
+            user_bet = int(message.text)
+            result = casino.bet(user_bet)
+            sum = result[0]
+            slots = result[1]
+            if db.balance(message.from_user.id)-user_bet>=0:
+                if sum>0:
+                    await db.update_balance(message.from_user.id, sum)
+                    await message.answer("Аппарат показал: " + slots + "\nВы выиграли: " + str(sum), reply_markup=kb.game)
+                    await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
 
 
-        else:
-            await db.update_balance(message.from_user.id, sum)
-            await message.answer("Аппарат показал: " + slots + "\nВы проиграли: "+str(sum*(-1)),reply_markup=kb.game)
-            await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
-    else:
-        await message.answer("Недостаточно денег на балансе: "+str(db.balance(message.from_user.id)))
-
-@dp.message_handler(text='1000')
-async def bet10(message: types.Message):
-    a = casino.bet(1000)
-    sum = a[0]
-    slots = a[1]
-    if db.balance(message.from_user.id)-1000>=0:
-        if sum>0:
-            await db.update_balance(message.from_user.id, sum)
-            await message.answer("Аппарат показал: " + slots + "\nВы выиграли: " + str(sum), reply_markup=kb.game)
-            await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
-
-
-        else:
-            await db.update_balance(message.from_user.id, sum)
-            await message.answer("Аппарат показал: " + slots + "\nВы проиграли: "+str(sum*(-1)),reply_markup=kb.game)
-            await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
-    else:
-        await message.answer("Недостаточно денег на балансе: "+str(db.balance(message.from_user.id)))
-
-@dp.message_handler(text='Выйти')
-async def game(message: types.Message):
-    await message.answer(f'Выберите ставку\nВаш баланс: {db.balance(message.from_user.id)}', reply_markup=kb.main)
+                else:
+                    await db.update_balance(message.from_user.id, sum)
+                    await message.answer("Аппарат показал: " + slots + "\nВы проиграли: "+str(sum*(-1)),reply_markup=kb.game)
+                    await message.answer(f"Ваш баланс: {db.balance(message.from_user.id)}")
+            else:
+                await message.answer("Недостаточно денег на балансе: " + str(db.balance(message.from_user.id)))
+        except ValueError:
+            await message.answer("Пожалуйста, введите целое число.")
 
 @dp.message_handler(text='Рейтинг')
 async def rating(message: types.Message):
-    await message.answer(db.rating(),reply_markup=kb.main)
+    await message.answer(db.rating(), reply_markup=kb.main)
 
 @dp.message_handler(commands='replenish')
 async def replenish(message: types.Message):
@@ -165,7 +126,7 @@ async def replenish(message: types.Message):
         args = message.text.split()
         id = args[1]
         sum = args[2]
-        db.replenish(id,sum)
+        db.replenish(id, sum)
         await message.answer("Успешное пополнение баланса пользователя: "+str(id)+" на: "+str(sum),reply_markup=kb.main)
         await bot.send_message(args[1], "Ваш баланс успешно пополнен на: "+str(sum))
     else:
